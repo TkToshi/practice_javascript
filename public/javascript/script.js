e9bab5ff1cc36ec7858e2dee293dbb1b232ad6ce
TkToshi <k_y_t_active@yahoo.co.jp>
2024-06-16 21:09:20 +0900

    3-1 カラピッカー修正

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -4,15 +4,15 @@ const color = document.querySelector('#colorPicker');
 //カラーピッカーを操作した時の一連の動作
 const colorBg = () => {
   //選択した色を背景色に設定
-  document.body.style.backgroundColor = color.ariaValueMax;
+  document.body.style.backgroundColor = color.value;
 
   //カラーコードを表示
   if (color.value === '#ffffff') {
-    text.textContent = 'カラーコード: ${color.value} (white)';
+    text.textContent = `カラーコード: ${color.value} (white)`;
   } else if (color.value === '#000000') {
-    text.textContent = 'カラーコード: ${color.value} (black)';
+    text.textContent = `カラーコード: ${color.value} (black)`;
   } else {
-    text.textContent = 'カラーコード: ${color.value}';
+    text.textContent = `カラーコード: ${color.value}`;
   }
 };
 
